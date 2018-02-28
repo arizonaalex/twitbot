5ae66d94b4566c4922b4c27eeb028f9119beff8b
Alex Bowen <alexbowen@Alexs-iMac.local>
2018-02-27 20:17:34 -0700

    fixed config issue

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'l7IwzFFXz3WVyJufevKDP0gYG',
+  consumer_secret:      'cHlF8gZNv0AYG59tKmOen0koYbzugdXV1PxABGsoUgTQfzw5eH',
+  access_token:         '724285656701095936-pS3DDx2p1NiOp1khRZyeCZePZSmEL43',
+  access_token_secret:  'NmwULa3P6fZ1jOEfoiWJNyqNjIN0PNLtVEV6qiM8H9zkp',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
